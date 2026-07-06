de8082f91d8315586d30ce57fea3ba50db4da35a
mario <mariomario007744@gmail.com>
2026-07-06 05:14:24 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/wajed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wajed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Wajed());
+}
+
+class Wajed extends StatelessWidget {
+  const Wajed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
